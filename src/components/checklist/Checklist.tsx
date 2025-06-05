33ef4682773cd5326955f4470a90b10320a12f62
DropAIproject <dropaitool@gmail.com>
2025-06-05 22:30:55 +0300

    Update Checklist.tsx

--- src/components/checklist/Checklist.tsx
+++ src/components/checklist/Checklist.tsx
@@ -1,13 +1,13 @@
-import { useProjects } from '../../hooks/useProjects';
-import { ChecklistItem } from './ChecklistItem';
-
+import { useProjects } from "../../hooks/useProjects";
+import { ChecklistItem } from "./ChecklistItem";
+import "./AirdropCard.css";
 export function Checklist() {
   const { projects } = useProjects();
 
   return (
-    <div className="airdrops">
+    <div className="airdrop-grid">
       {projects.map((project) => (
-        <ChecklistItem key={project.id} project={project} />
+        <ChecklistItem key={project._id} project={project} />
       ))}
     </div>
   );
